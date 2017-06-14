18a826481409819413b751ebf435240ce48f2f58
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-06-14 16:07:18 -0500

    completed step 4

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -11,6 +11,16 @@ module.exports = {
   },
 
   getOccupations : function(req, res, next){
+    if(req.query.order){
+      var order = req.query.order
+      if(order == 'asc'){
+        var filtered = user.occupations.reverse()
+        return res.status(200).json(filtered)
+      } else if(order == 'desc'){
+        var filtered = user.occupations.sort()
+        return res.status(200).json(filtered)
+      }
+    }
     return res.status(200).json(user.occupations)
   },
 
